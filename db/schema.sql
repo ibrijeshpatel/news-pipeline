cae1dd7df2493038dfc2f348847d95fcb5483156
Brijesh Patel <136248611+ibrijeshpatel@users.noreply.github.com>
2025-07-18 01:19:25 +0530

    Update schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,8 @@
+CREATE TABLE IF NOT EXISTS articles (
+    article_id TEXT PRIMARY KEY,
+    url TEXT NOT NULL,
+    title TEXT NOT NULL,
+    publication_ts DATETIME NOT NULL,
+    source TEXT NOT NULL
+);
 
